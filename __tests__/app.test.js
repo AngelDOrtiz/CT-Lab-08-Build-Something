926847bb87eed56a36a46f86c5383bd82cb6ab85
AngelDOrtiz <aortizghs@gmail.com>
2021-06-22 12:07:34 -0700

    second test set up, not passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,4 +24,39 @@ describe('horoscope routes', () => {
       'luckyTime': '10am'
     });
   });
+
+  it('finds all horoscopes via GET', async () => {
+    const aquarius = await Horoscope.insert({
+      'currentDate': 'June 22, 2021',
+      'horoscope': 'It\'s a good day to do nothing -- you\'re waiting for a new burst of inspiration, and it\'s coming tomorrow or thereabouts. If you can blow off your appointments, that would be perfect.',
+      'compatibility': 'Gemini',
+      'mood': 'Cool',
+      'color': 'Spring Green',
+      'luckyNumber': '13',
+      'luckyTime': '10am'
+    });
+
+    const sagittarius = await Horoscope.insert({
+      'currentDate': 'June 22, 2021',
+      'horoscope': 'Your closest connections are more important than ever today, so make sure that you\'re in touch with your partners -- business, romantic and other. It\'s a good time to clear the ground between you.',
+      'compatibility': 'Aries',
+      'mood': 'Social',
+      'color': 'Orchid',
+      'luckyNumber': '34',
+      'luckyTime': '3pm'
+    });
+
+    const aries = await Horoscope.insert({
+      'currentDate': 'June 22, 2021',
+      'horoscope': 'Risks are rewarded today -- though not all of them pay off in the way you might expect them to! Go ahead and make a few plays that feel close to the edge and see how far you can take it.',
+      'compatibility': 'Cancer',
+      'mood': 'Productive',
+      'color': 'Blue',
+      'luckyNumber': '56',
+      'luckyTime': '4pm'
+    });
+
+    const res = await request(app).get('/api/v1/horoscopes');
+    expect(res.body).toEqual({ aquarius, sagittarius, aries });
+  });
 });
